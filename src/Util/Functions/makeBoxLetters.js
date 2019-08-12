9bc9f453ea58aebbb6b808a7f1d023de7d9501f7
Apacheli <43933794+Apacheli@users.noreply.github.com>
2019-08-12 01:47:53 -0500

    Improve code

--- src/Util/Functions/makeBoxLetters.js
+++ src/Util/Functions/makeBoxLetters.js
@@ -1,37 +1,17 @@
 function makeBoxLetter(word) {
-      const boxWords = []
-      word.split("").forEach(f => {
-            if (f === ' ') {
-                  boxWords.push('   ')
-            } else if (isNaN(f) === false) {
-                  if (f === '0') {
-                        boxWords.push(':zero:')
-                  } else if (f === '1') {
-                        boxWords.push(':one:')
-                  } else if (f === '2') {
-                        boxWords.push(':two:')
-                  } else if (f === '3') {
-                        boxWords.push(':three:')
-                  } else if (f === '4') {
-                        boxWords.push(':four:')
-                  } else if (f === '5') {
-                        boxWords.push(':five:')
-                  } else if (f === '6') {
-                        boxWords.push(':six:')
-                  } else if (f === '7') {
-                        boxWords.push(':seven:')
-                  } else if (f === '8') {
-                        boxWords.push(':eight:')
-                  } else if (f === '9') {
-                        boxWords.push(':nine:')
-                  }
-            } else if (f.match(/[a-z]/i)) {
-                  boxWords.push(`:regional_indicator_${f}:`)
-            } else {
-                  boxWords.push(f)
-            }
-      })
-      return boxWords.join("")
+      const numbers = [
+            'one',
+            'two',
+            'three',
+            'four',
+            'five',
+            'six',
+            'seven',
+            'eight',
+            'nine',
+            'zero'
+      ]
+      return word.replace(/\w/g, (w) => `:${isNaN(w) ? `regional_indicator_${w}` : numbers[w - 1]}:`)
 }
 
 module.exports = makeBoxLetter
