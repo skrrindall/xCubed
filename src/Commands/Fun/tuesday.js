684a08e9bc13c17894339bf0de25aefee60bc6d4
Oklar <36753435+LandenStephenss@users.noreply.github.com>
2019-10-10 21:48:55 -0500

    Tuesday?

---
+++ src/Commands/Fun/tuesday.js
@@ -0,0 +1,21 @@
+module.exports = {
+    Triggers: ['isittuesday', 'is-it-tuesday', 'tuesday'],
+    Description: 'What day is it?',
+    Category: 'fun',
+    Usage: '{c}',
+    Permissions: {
+      User: [],
+      Bot: ['SEND_MESSAGES']
+    },
+    Options: {
+      Dev: false,
+      NSFW: false,
+      Cooldown: {
+        Enabled: true,
+        Time: 0
+      },
+    },
+    Run: async (client, message, paramaters) => {
+      message.channel.send(`Today **is${new Date().getDay() === 2 ? '' : ' not'}** Tuesday.`);
+    }
+  }
