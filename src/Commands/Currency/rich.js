ae0aff88aa64b9f09e7e04b1d542fc8763eccd2b
Oklar <36753435+LandenStephenss@users.noreply.github.com>
2019-10-13 00:02:48 -0500

    rich?

---
+++ src/Commands/Currency/rich.js
@@ -0,0 +1,26 @@
+module.exports = {
+    Triggers: ['rich', 'moneylb', 'richleaderboard', 'richest', 'richs'],
+    Description: 'See whos the richest of all!',
+    Category: 'currency',
+    Usage: '{c}',
+    Permissions: {
+      User: [],
+      Bot: ['EMBED_LINKS', 'SEND_MESSAGES']
+    },
+    Options: {
+      Dev: false,
+      NSFW: false,
+      Cooldown: {
+        Enabled: true,
+        Time: 0
+      },
+    },
+    Run: async (client, message, paramaters) => {
+    const user = message.mentions.members.first() || message.member
+    const filtered = client.Credits.filter( p => message.guild.members.has(p.user)).array()
+    const sort = filtered.sort((a, b) => b.Wallet - a.Wallet)
+    const top5 = sort.splice(0, 5)
+    console.log(top5)
+    }
+  }
+
