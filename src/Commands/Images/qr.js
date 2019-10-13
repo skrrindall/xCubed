e6a711da7e0d10037f270641011626de26f6747c
Oklar <36753435+LandenStephenss@users.noreply.github.com>
2019-10-13 00:26:18 -0500

    Update qr.js

--- src/Commands/Images/qr.js
+++ src/Commands/Images/qr.js
@@ -17,10 +17,13 @@ module.exports = {
       },
     },
     Run: async (client, message, paramaters) => {
+        if(!paramaters[0]) {
+             message.channel.send(require('../../Configurations/Errors.json').NoArguments)   
+        } else {
     const Embed = new RichEmbed()
     .setColor('RED')
-    .setImage(`https://api.qrserver.com/v1/create-qr-code/?data=${encodeURIComponent(args)}`)
+    .setImage(`https://api.qrserver.com/v1/create-qr-code/?data=${encodeURIComponent(paramaters.join(' '))}`)
     message.channel.send(Embed)
-        
+        }
     }
   }
