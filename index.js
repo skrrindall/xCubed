636d5c26bb0be974883a698f9e92c4f23754dbf0
Oklar <36753435+LandenStephenss@users.noreply.github.com>
2019-10-15 08:31:33 -0500

    comments

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
+// Instead of loading the entire discord.js module, I choose to load everything that i need for this one snippet of code.
 const {
     Client,
     login,
     Collection
 } = require('discord.js')
+// First we create the client
 const client = new Client()
+// Next we login and grab our token straight from our file so that it is not saved anywhere on the bot.
 client.login(require('./src/Configurations/Config.json').token)
+// Here we make new collections for all of the commands
 client.triggers = new Collection()
 client.commands = new Collection()
+// We have commands used here as well. This is for the status command
 client.commandsUsed = 0
-client.queue = new Map()
-require('./src/Util/Misc/EventLoadingHandler.js')(client)
+// Here is were we load all of the events.
+require('./src/Util/Misc/EventLoadingHandler.js')(client) 
+// Soon i will be switching to a custom DataBase but for now i am using enmap made by evie.codes
 const Enmap = require('enmap')
+// Below this is where i load all of the DataBases that stores everything
 client.Prefix = new Enmap({
     name: 'prefix'
 })
