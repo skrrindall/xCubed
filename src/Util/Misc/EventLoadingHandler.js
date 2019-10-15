0426e96c54627150cdde6f5077c43d305a172b63
Oklar <36753435+LandenStephenss@users.noreply.github.com>
2019-10-15 08:33:14 -0500

    Update EventLoadingHandler.js

--- src/Util/Misc/EventLoadingHandler.js
+++ src/Util/Misc/EventLoadingHandler.js
@@ -1,3 +1,4 @@
+// This is our function to Load events, it is a simple onliner and it works perfectly
 const Load = (event) => require(`../Events/${event}`);
 module.exports = (client) => {
     client.on('ready', () => Load('Ready')(client))
