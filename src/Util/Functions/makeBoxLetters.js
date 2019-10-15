8bba82d7f78722c1ba07a121db41bea5da8b6b55
Oklar <36753435+LandenStephenss@users.noreply.github.com>
2019-10-15 08:38:36 -0500

    Update makeBoxLetters.js

--- src/Util/Functions/makeBoxLetters.js
+++ src/Util/Functions/makeBoxLetters.js
@@ -1,4 +1,5 @@
 function makeBoxLetter(word) {
+      // This is basically a list of all of the possible number emojis that there is
       const numbers = [
             'one',
             'two',
@@ -11,6 +12,7 @@ function makeBoxLetter(word) {
             'nine',
             'zero'
       ]
+      // This is checking if each character is an alpabetic character, if it is then it is placed as an emoji and if it is not then it is just returned as is
       return word.replace(/\w/g, (w) => `:${isNaN(w) ? `regional_indicator_${w}` : numbers[w - 1]}:`)
 }
 
