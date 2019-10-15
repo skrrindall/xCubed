959cfef7126790f1471f1382e25f7a583d687426
Oklar <36753435+LandenStephenss@users.noreply.github.com>
2019-10-15 13:14:21 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Instead of loading the entire discord.js module, I choose to load everything that i need for this one snippet of code.
 const {
-    Client,
+	Client,
     login,
     Collection
 } = require('discord.js')
@@ -14,25 +14,25 @@ client.commands = new Collection()
 // We have commands used here as well. This is for the status command
 client.commandsUsed = 0
 // Here is were we load all of the events.
-require('./src/Util/Misc/EventLoadingHandler.js')(client) 
+require('./src/Util/Misc/EventLoadingHandler.js')(client)
 // Soon i will be switching to a custom DataBase but for now i am using enmap made by evie.codes
 const Enmap = require('enmap')
 // Below this is where i load all of the DataBases that stores everything
 client.Prefix = new Enmap({
-    name: 'prefix'
+	name: 'prefix'
 })
 client.Points = new Enmap({
-    name: 'Points'
+	name: 'Points'
 })
 client.Credits = new Enmap({
-    name: 'credits'
+	name: 'credits'
 })
 client.PP = new Enmap({
-    name: 'PP'
+	name: 'PP'
 })
 client.HowGay = new Enmap({
-    name: 'HowGay'
+	name: 'HowGay'
 })
 client.DankRate = new Enmap({
-    name: 'DankRate'
+	name: 'DankRate'
 })
