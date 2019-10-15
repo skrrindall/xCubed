40f66f4dfe2acff5bbe895c854c98e5adb3cd6e0
Oklar <36753435+LandenStephenss@users.noreply.github.com>
2019-10-15 08:36:03 -0500

    Update firstUpper.js

--- src/Util/Functions/firstUpper.js
+++ src/Util/Functions/firstUpper.js
@@ -1,6 +1,9 @@
 function firstUpper(string) {
+  // This variable grabs the string, splits all the letters up, grabs the first one, and then makes it uppercase
 const first = string.split("")[0].toUpperCase()
+// This variable takes the string, splits all the letters up, gets rid of the last letter
 const rest = string.split("").slice(1).join("")
+// This adds the two strings together
 return first + rest
 }
 
