95e0b4deb7d33e5af4c72d731e32673f0213ae18
Oklar <36753435+LandenStephenss@users.noreply.github.com>
2019-10-15 08:33:58 -0500

    Update Write.js

--- src/Util/Functions/Write.js
+++ src/Util/Functions/Write.js
@@ -1,5 +1,7 @@
 const Write = (args, type) => {
+      // This is the types of logging that we have. They are all differnt colors
       var types = ['36m[Bot]    ', '91m[Warning]', '31m[Error]', '92m[Debug]  ']
+      // This peice of code is basically a special console.log()
       process.stdout.write(`\u001b[${types[type]}\u001b[39m || ${args}\n`)
 }
 module.exports = Write
